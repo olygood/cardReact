ee94004bad6d3ccefb7925a66e2b37f50f66bead
olygood <olygood@outlook.com>
2022-08-16 18:31:33 +0200

    mise en page1 ok

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hello react</h1>
-       
-      </header>
+      
+        <div className="header">
+          <h1>je suis le header</h1>
+        </div>
+          <div className='main'>
+            
+          </div>
+         
+        <div className="footer">
+         <h3>je suis le footer</h3>         
+        </div>
+      
     </div>
   );
 }
